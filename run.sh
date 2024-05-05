21c9fef7d66e9d0891045cc8393383a7cf0584b8
Nadman A. Khan <nadman.ash.khan@gmail.com>
2024-05-05 17:30:39 +0600

    Convenience script to build and run the full application

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+make && \
+./simulator/qpmu-simulator \
+--v "$1" --p "$2" \
+| \
+./estimator/qpmu-estimator --window 24 \
+--vscale 0.3205000 --voffset 0 \
+--iscale 0.0073663 --ioffset 0.0357669 \
+| \
+./gui/qpmu-gui
