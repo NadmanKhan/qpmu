c65a737ced8687bc92ac8805803d2ea00671c556
Nadman A. Khan <nadman.ash.khan@gmail.com>
2024-05-06 15:48:17 +0600

    Fix possible bug in run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 
 make && \
 ./simulator/qpmu-simulator \
---v "$1" --p "$2" \
-| \
-./estimator/qpmu-estimator --window 24 \
---vscale 0.3205000 --voffset 0 \
---iscale 0.0073663 --ioffset 0.0357669 \
-| \
-./gui/qpmu-gui
+--v "$1" \
+--p "$2" \
+| ./estimator/qpmu-estimator \
+--window 24 \
+--vscale 0.3205000 \
+--voffset 0 \
+--iscale 0.0073663 \
+--ioffset 0.0357669 \
+| ./gui/qpmu-gui
