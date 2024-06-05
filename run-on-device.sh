49da1de3143ea6f76c8144827aaed43be27aa371
Nadman A. Khan <nadman.ash.khan@gmail.com>
2024-06-05 16:31:41 +0600

    Add script to run on device

---
+++ run-on-device.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is intended to be run on the device
+
+make &&
+    ~/1_cpptest/pmu-adc/host_rpmsg_mcp3208 |
+    ./estimator/qpmu-estimator \
+        --window 24 \
+        --phasor-est fft \
+        --freq-est cpd \
+        --vscale 0.3205000 \
+        --voffset 0 \
+        --iscale 0.0073663 \
+        --ioffset 0 \
+        --infmt s |
+        ./gui/qpmu-gui
