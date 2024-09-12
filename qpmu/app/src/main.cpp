257d2d23dbef49286e9cd75426bfb4069e903595
Nadman A. Khan <nadman.ash.khan@gmail.com>
2024-09-12 10:57:32 +0600

    remove QT_IM_MODULE env

--- qpmu/app/src/main.cpp
+++ qpmu/app/src/main.cpp
@@ -3,13 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     App app(argc, argv);
 
     app.mainWindow()->show();
-
+    
     return app.exec();
 }
