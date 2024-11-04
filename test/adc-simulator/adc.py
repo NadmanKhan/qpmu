b2b70619715b7a443a0bc5a9deae4ede917bf34f
Nadman A. Khan <nadman.ash.khan@gmail.com>
2024-11-04 13:12:38 +0600

    Update ADC simulator

--- test/adc-simulator/adc.py
+++ test/adc-simulator/adc.py
@@ -71,73 +71,153 @@ class TimestampedADC:
 
         self.sampling_period_s = 1 / self.sampling_rate_hz
         self.max_value = 2**self.resolution_bits - 1
-        self.scale = self.max_value / self.reference_voltage_v
+        self.scale = (self.max_value / 2) / self.reference_voltage_v
 
     def generate_sample(self, signal: AnalogSignal, noise: float, time_s: float) -> int:
-        noise_amount = noise * random.uniform(-1, 1) * self.reference_voltage_v
-        value = signal(time_s) + noise_amount
-        result = round(value * self.scale + self.max_value)  # scale and offset to ADC range
-        result = max(0, min(result, self.max_value))  # clamp to ADC range
+        result = math.floor(signal(time_s) * self.scale + self.max_value // 2)  # scale and offset to ADC range
+        # result += noise * random.uniform(-1, 1) * self.reference_voltage_v
+        result = max(0, min(int(result), self.max_value))  # clamp to ADC range
         return result
 
-    def stream(self):
+    def stream_raw(self):
         while True:
             time.sleep(self.sampling_period_s * 0.9)
             time_ns = time.time_ns()
-            yield TimestampedADC.Sample(
+            yield TimestampedADC.RawBuffer(
                 channel_values=tuple(self.generate_sample(s, n, time_ns / 1e9) for s, n in self.channels),
-                timestamp_us=time_ns // 1000
+                timestamp_nsec=time_ns // 1000
             )
-
+    
+    def stream_samples(self):
+        last_time_usec = 0
+        while True:
+            time.sleep(self.sampling_period_s * 0.9)
+            time_usec = time.time_ns() // 1000
+            sequence_number = 0
+            yield TimestampedADC.Sample(
+                sequence_number=sequence_number,
+                channel_values=tuple(self.generate_sample(s, n, time_usec / 1e6) for s, n in self.channels),
+                timestamp_usec=time_usec,
+                timedelta_usec=time_usec - last_time_usec
+            )
+            sequence_number += 1
+            last_time_usec = time_usec
+    
     @dataclass
     class Sample:
+        STRUCT_FORMAT = "@Q6H2Q" # 1 sequence number (64-bit), 6 ADC values (16-bit), 1 system timestamp (microseconds; 64-bit), 1 time delta since previous sample (microseconds; 64-bit)
+        BUFSIZE = struct.calcsize(STRUCT_FORMAT)
 
-        STRUCT_FORMAT = "@Q180H"
-        SAMPLE_SIZE = struct.calcsize(STRUCT_FORMAT)
-
-        CSV_FORMAT = "ts={timestamp_us},\tch0={channel_values[0]:4},ch1={channel_values[1]:4},ch2={channel_values[2]:4},\
-            ch3={channel_values[3]:4},ch4={channel_values[4]:4},ch5={channel_values[5]:4},"
+        CSV_FORMAT = "seq={sequence_number},\t ch0={channel_values[0]:4}, ch1={channel_values[1]:4}, ch2={channel_values[2]:4}, ch3={channel_values[3]:4}, ch4={channel_values[4]:4}, ch5={channel_values[5]:4},\t ts={timestamp_usec},\t delta={timedelta_usec}"
 
         def __init__(
             self,\
-            timestamp_us: int,
-            channel_values: Tuple[int]
+            sequence_number: int,\
+            channel_values: Tuple[int],\
+            timestamp_usec: int,\
+            timedelta_usec: int
         ):
-            assert isinstance(timestamp_us, int), "Timestamp must be an integer"
+            assert isinstance(sequence_number, int), "Sequence number must be an integer"
             assert all(isinstance(value, int) for value in channel_values), "Channel values must be integers"
             assert len(channel_values) == 6, "Channel values must have 6 elements"
+            assert isinstance(timestamp_usec, int), "Timestamp must be an integer"
+            assert isinstance(timedelta_usec, int), "Delta must be an integer"
 
-            self.timestamp_us = timestamp_us
+            self.sequence_number = sequence_number
             self.channel_values = channel_values
+            self.timestamp_usec = timestamp_usec
+            self.timedelta_usec = timedelta_usec
 
         def __bytes__(self) -> bytes:
             b = struct.pack(
                 TimestampedADC.Sample.STRUCT_FORMAT,
-                self.timestamp_us,
+                self.sequence_number,
                 *self.channel_values,
-                *([0] * (180 - len(self.channel_values)))
+                self.timestamp_usec,
+                self.timedelta_usec
             )
             return b
-
+        
         def __str__(self) -> str:
             return TimestampedADC.Sample.CSV_FORMAT.format(
-                timestamp_us=self.timestamp_us,
+                sequence_number=self.sequence_number,
                 channel_values=self.channel_values,
+                timestamp_usec=self.timestamp_usec,
+                timedelta_usec=self.timedelta_usec
             )
-
+        
         @classmethod
         def from_bytes(cls, data: bytes) -> "TimestampedADC.Sample":
             values = struct.unpack(cls.STRUCT_FORMAT, data)
-            timestamp_us = values[0]
+            sequence_number = values[0]
             channel_values = values[1:7]
-            return cls(timestamp_us, channel_values)
-
+            timestamp_usec = values[7]
+            timedelta_usec = values[8]
+            return cls(sequence_number, channel_values, timestamp_usec, timedelta_usec)
+        
         @classmethod
         def from_str(cls, data: str) -> "TimestampedADC.Sample":
             def value_from_kvstring(kvs: str) -> int:
                 return int(kvs.split("=")[1])
 
             kvstrings = data.split(",")
-            timestamp_us = value_from_kvstring(kvstrings[0])
+            sequence_number = value_from_kvstring(kvstrings[0])
             channel_values = tuple(value_from_kvstring(kvs) for kvs in kvstrings[1:7])
-            return cls(timestamp_us, channel_values)
+            timestamp_usec = value_from_kvstring(kvstrings[7])
+            timedelta_usec = value_from_kvstring(kvstrings[8])
+            return cls(sequence_number, channel_values, timestamp_usec, timedelta_usec)
+
+
+    @dataclass
+    class RawBuffer:
+
+        STRUCT_FORMAT = "@Q180H" # 1 PRU timestamp (nanoseconds; 64-bit), 6 ADC values (16-bit), rest padding
+        BUFSIZE = struct.calcsize(STRUCT_FORMAT)
+
+        CSV_FORMAT = "ts={timestamp_nsec},\t ch0={channel_values[0]:4}, ch1={channel_values[1]:4}, ch2={channel_values[2]:4}, ch3={channel_values[3]:4}, ch4={channel_values[4]:4}, ch5={channel_values[5]:4}"
+
+        def __init__(
+            self,\
+            timestamp_nsec: int,
+            channel_values: Tuple[int]
+        ):
+            assert isinstance(timestamp_nsec, int), "Timestamp must be an integer"
+            assert all(isinstance(value, int) for value in channel_values), "Channel values must be integers"
+            assert len(channel_values) == 6, "Channel values must have 6 elements"
+
+            assert TimestampedADC.RawBuffer.BUFSIZE == 368, "Sample size must be 368 bytes"
+
+            self.timestamp_nsec = timestamp_nsec
+            self.channel_values = channel_values
+
+        def __bytes__(self) -> bytes:
+            b = struct.pack(
+                TimestampedADC.RawBuffer.STRUCT_FORMAT,
+                self.timestamp_nsec,
+                *self.channel_values,
+                *([0] * max(0, TimestampedADC.RawBuffer.BUFSIZE - len(self.channel_values)))
+            )
+            return b
+
+        def __str__(self) -> str:
+            return TimestampedADC.RawBuffer.CSV_FORMAT.format(
+                timestamp_nsec=self.timestamp_nsec,
+                channel_values=self.channel_values,
+            )
+
+        @classmethod
+        def from_bytes(cls, data: bytes) -> "TimestampedADC.RawBuffer":
+            values = struct.unpack(cls.STRUCT_FORMAT, data)
+            timestamp_nsec = values[0]
+            channel_values = values[1:7]
+            return cls(timestamp_nsec, channel_values)
+
+        @classmethod
+        def from_str(cls, data: str) -> "TimestampedADC.RawBuffer":
+            def value_from_kvstring(kvs: str) -> int:
+                return int(kvs.split("=")[1])
+
+            kvstrings = data.split(",")
+            timestamp_nsec = value_from_kvstring(kvstrings[0])
+            channel_values = tuple(value_from_kvstring(kvs) for kvs in kvstrings[1:7])
+            return cls(timestamp_nsec, channel_values)
--- test/adc-simulator/main.py
+++ test/adc-simulator/main.py
@@ -4,18 +4,21 @@ import argparse
 import math
 import os
 from pathlib import Path
+import sys
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the ADC simulation")
     parser.add_argument("--no-gui", default=False, action="store_true", help="Run without GUI")
     parser.add_argument("--sampling-rate", "-r", type=int, default=1200, help="Sampling rate in Hz")
-    parser.add_argument("--bits", "-b", type=int, default=8, help="ADC resolution in bits")
-    parser.add_argument("--noise", "-z", type=float, default=0.25, help="Noise")
-    parser.add_argument("--frequency", "-f", type=float, default=50, help="Signal frequency in Hz")
+    parser.add_argument("--bits", "-b", type=int, default=12, help="ADC resolution in bits")
+    parser.add_argument("--noise", "-z", type=float, default=0.1, help="Noise")
+    parser.add_argument("--frequency", "-f", type=float, default=49.5, help="Signal frequency in Hz")
     parser.add_argument("--voltage", "-v", type=float, default=240, help="Maximum voltage in volts")
     parser.add_argument("--current", "-i", type=float, default=10, help="Maximum current in amperes")
     parser.add_argument("--phasediff", "-p", type=float, default=15, help="V-I phase difference in degrees")
+    parser.add_argument("--binary", "-B", default=False, action="store_true", help="Output binary data")
+    parser.add_argument("--raw", "-R", default=False, action="store_true", help="Output raw data")
     parser.add_help = True
 
     args = parser.parse_args()
@@ -72,20 +75,49 @@ if __name__ == "__main__":
                 pass
             
             pipe_path = os.getenv("ADC_STREAM")
-            if not os.getenv("ADC_STREAM"):
-                raise ValueError("ADC_STREAM environment variable not set")
-            pipe_path = Path(pipe_path).absolute()
-            print(f"Pipe path: {pipe_path}")
-            
-            if os.path.exists(pipe_path):
-                os.remove(pipe_path)
-            os.mkfifo(pipe_path)
-            with open(pipe_path, "wb") as pipe:
-                for sample in adc.stream():
-                    pipe.write(bytes(sample))
 
-            for sample in adc.stream():
-                print(sample)
+            if not pipe_path:
+                if args.binary:
+                    if args.raw:
+                        for rawbuf in adc.stream_raw():
+                            sys.stdout.buffer.write(bytes(rawbuf))
+                    else:
+                        for sample in adc.stream_samples():
+                            sys.stdout.buffer.write(bytes(sample))
+                else:
+                    if args.raw:
+                        for rawbuf in adc.stream_raw():
+                            print(rawbuf)
+                    else:
+                        print("Time, VA, VB, VC, IA, IB, IC")
+                        for sample in adc.stream_samples():
+                            print(sample)
+            
+            else:
+                pipe_path = Path(pipe_path).absolute()
+                print(f"Pipe path: {pipe_path}")
+                
+                if os.path.exists(pipe_path):
+                    os.remove(pipe_path)
+                os.mkfifo(pipe_path)
+
+                if args.binary:
+                    with open(pipe_path, "wb") as pipe:
+                        if args.raw:
+                            for rawbuf in adc.stream_raw():
+                                pipe.write(bytes(rawbuf))
+                        else:
+                            for sample in adc.stream_samples():
+                                pipe.write(bytes(sample))
+                
+                else:
+                    with open(pipe_path, "w") as pipe:
+                        if args.raw:
+                            for rawbuf in adc.stream_raw():
+                                pipe.write(f"{rawbuf}\n")
+                        else:
+                            for sample in adc.stream_samples():
+                                pipe.write(f"{sample}\n")
 
         except Exception as e:
             print(f"Error: {e}")
